6ea88be4760df07a07ec84f82cf7e43d15179ed4
Robert Porter <robert.porter@rakuten.com>
2020-04-30 15:31:14 +0900

    Cleaning up Signal a little

--- src/Router/Signal.purs
+++ src/Router/Signal.purs
@@ -18,23 +18,23 @@ create :: forall a. a -> Effect (Signal a)
 create init = ado
   value <- Ref.new init
   subscribers <- Ref.new []
-  in Signal
-    { read: Ref.read value
-    , write:
-        \a -> do
-          Ref.write a value
-          Ref.read subscribers >>= traverse_ \k -> k a
-    , subscribe:
-        \k -> do
-          unsubscribing <- Ref.new false
-          let
-            subscriber = \a -> unlessM (Ref.read unsubscribing) (k a)
-          Ref.modify_ (flip snoc subscriber) subscribers
-          Ref.read value >>= k
-          pure do
-            Ref.write true unsubscribing
-            Ref.modify_ (deleteBy unsafeRefEq subscriber) subscribers
-    }
+  let
+    read = Ref.read value
+
+    write a = do
+      Ref.write a value
+      Ref.read subscribers >>= traverse_ \k -> k a
+
+    subscribe k = do
+      unsubscribing <- Ref.new false
+      let
+        subscriber = \a -> unlessM (Ref.read unsubscribing) (k a)
+      Ref.modify_ (flip snoc subscriber) subscribers
+      Ref.read value >>= k
+      pure do
+        Ref.write true unsubscribing
+        Ref.modify_ (deleteBy unsafeRefEq subscriber) subscribers
+  in Signal { read, write, subscribe }
 
 read :: forall a. Signal a -> Effect a
 read (Signal signal) = signal.read
